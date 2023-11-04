9f508a856f90c263435eb73ea976a4810a5ea2cc
Seohabin6078 <habin6078@gmail.com>
2023-11-04 23:55:14 +0900

    create project

---
+++ src/main/java/com/modooboard/ModooBoardApplication.java
@@ -0,0 +1,13 @@
+package com.modooboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModooBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModooBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modooboard/ModooBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modooboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModooBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
